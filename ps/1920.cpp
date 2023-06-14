159c55037c35133c6bddb3a5b040fcaf34e90b43
jejemerong <9708jjw@gmail.com>
2023-06-14 22:58:55 +0900

    Add: 1920 수 찾기

---
+++ ps/1920.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int targetNum, elementNum, result;
+vector<int> targets, elements;
+
+int main()
+{
+  freopen("input.txt", "r", stdin);
+  cin << targetNum << endl;
+  for(int  i = 0; i < targetNum; i++){
+    cin >> targets[i];
+  }
+
+  return 0;
+}
+
+int main() {
+	int n;
+ 
+	while (cin >> n) {
+		cout << n << endl;
+	}
+ 
+	cout << "end";
+}
