55301d0750152c96743d108bbf45c5e0fe4af6a0
jejemerong <9708jjw@gmail.com>
2023-06-17 23:06:51 +0900

    Fix: 1753 시간 초과 해결

--- ps/1753.cpp
+++ ps/1753.cpp
@@ -1,49 +1,61 @@
 #include <iostream>
 #include <vector>
 #include <queue>
+
 using namespace std;
 
-/**
- * 1. 인접 리스트: (인접 노드, 방향, 가중치) 저장
- * 2. 시작 노드에서 i 번째 노드까지 인접 노드에 대한 조합 값 조사 (방향, 가중치)
- * 3. 해당 배열 정렬하여 최단 경로 출력
-*/
+#define INF 99999999
 
-int vertices, edges, start;
+int v, e, start;
 int x, y, z;
-vector<vector<vector<int> > > graph; // 가중치 포함
-// TODO: vector 말고 queue 로 풀어보기
-vector<int> minArr;
+vector<vector<pair <int, int > > > graph;
+int minArr[20010]; // TODO: minArr 의 MAX 길이 기준
 
-#define INF
+void dijkstra(){
+  priority_queue<pair<int, int> > disNode;
 
-void dijkstra (int start, int vertices, vector<vector<vector<int>>>&graph) {
-    minArr[0] = 0;
-    minArr.resize(vertices);
-    for(int i = 0; i < vertices; i++){
-        if(graph[start][i].size() > 0) {
-            minArr[i] = graph[start][i].at(0); // v.[i] (operator []) 써도 
-            cout << minArr[i] << "\n";
-        }else {
-            cout << INF"\n";
-        }
-    }
-}
-
-int main()
-{
-  ios::sync_with_stdio(false);
-  cin.tie(nullptr);
-//   freopen("input.txt", "r", stdin);
-
-  scanf("%d %d", vertices, edges);
-  scanf("%d", start);
-  graph.resize(vertices+1);
-  for(int i = 0; i < edges; i++){
-    scanf("%d %d %d", x, y, z);
-    graph[x][y].push_back(z);
+// TODO: 초기값 다 INF 말고 인접 노드 대로 넣을 순 없나?
+  for(int i = 1; i <= v; i++){
+    minArr[i] = INF;
   }
 
-  dijkstra(start, vertices, graph);
+  minArr[start] = 0;
+  disNode.push(make_pair(0, start)); // 거리가 우선으로 정렬되기 위해 0이 앞에 옴.
+
+  while(!disNode.empty()){
+    // 현재 방문 노드와 현재의 최단 경로
+    int current = disNode.top().second; // 인접 노드 정보
+    int distance = -disNode.top().first; // 최소 힙으로 저장된(-) 해당 노드의 최단 거리를 양수로
+    disNode.pop(); // 큐에서 제거
+    for(int i = 0; i < graph[current].size(); i++){
+      int next = graph[current][i].first; // 인접 노드 정보
+      int nextDistance = graph[current][i].second; // 가중치
+      if(minArr[next] > nextDistance + distance) { // 현재 저장된 최단 거리와 인접 노드 가중치를 계산한 값 비교
+        minArr[next] = nextDistance + distance; // 맞다면 대체
+        disNode.push(make_pair(-minArr[next], next)); // 최소 힙으로 큐에 추가
+      }
+    }
+  }
+}
+
+int main(){
+  // freopen("input.txt", "r", stdin);
+  scanf("%d %d", &v, &e);
+  scanf("%d", &start);
+
+  graph.resize(v + 1);
+  for(int i = 0 ; i < e; i++){
+    scanf("%d %d %d", &x, &y, &z);
+    graph[x].push_back(make_pair(y, z));
+  }
+
+  dijkstra();
+  for(int i = 1; i <= v; i++){
+    if(minArr[i] == INF) {
+      printf("INF\n");
+    }else {
+      printf("%d\n", minArr[i]);
+    }
+  }
   return 0;
 }
