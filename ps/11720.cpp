c6c3b790738dfd1f60d2adb6a4d740e0eb2d4efb
jejemerong <9708jjw@gmail.com>
2023-07-17 03:16:30 +0900

    Chores: 주석 추가

--- ps/11720.cpp
+++ ps/11720.cpp
@@ -14,7 +14,8 @@ int main()
 
     int sum = 0;
     for(int i = 0; i < numbers.length(); i++){
-        sum += numbers[i] - '0';
+        // numbers[i] 는 문자형이기 때문에 이를 숫자형으로 바꿔주려면 49 또는 '0' 을 빼면 됨. 이는 ASCII 코드에 의해 매핑된 값임.
+        sum += numbers[i] - '0'; 
     }
     cout << sum << "\n";
     return 0;
