ecb0efe42089f7b2971fc2e6e0d5701d49a679b3
jejemerong <9708jjw@gmail.com>
2023-07-17 00:39:48 +0900

    Chores: delete 쓸데없는

--- cppgrammer/pair.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-    pair <int, int> position (1, 2);
-    // 한 요소만 출력 시, 
-    cout << position.first << position.second << endl;
-    position.swap(position.first, position);
-}
