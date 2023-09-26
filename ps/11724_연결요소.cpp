54ae7459cfdad26985e78a747708c635da5874e2
jejemerong <9708jjw@gmail.com>
2023-09-26 23:13:13 +0900

    Fix: for 인덱스 접근 방식 수정

--- ps/11724_연결요소.cpp
+++ ps/11724_연결요소.cpp
@@ -4,10 +4,20 @@
 using namespace std;
 
 // Type Def
-static vector<vector<int > > Node;
+static vector<vector<int > > Graph;
 static vector <bool> isVisited;
 
-void DFS(int v);
+void DFS(int v, int count) {
+    if(isVisited[v]) return;
+    isVisited[v] = true;
+
+    for(int i = 0; i < Graph[v].size();i++){
+        int y = Graph[v][i];
+        if(isVisited[y] == false) {
+            DFS(y, count);
+        }
+    }
+}
 
 int main()
 {
@@ -15,39 +25,25 @@ int main()
     int N, M;
     scanf("%d %d", &N, &M);
     int count = 0;
-    Node.resize(N + 1);
+    Graph.resize(N + 1);
     isVisited = vector<bool>(N+1, false);
 
     // vector Init
     int u, v;
     for(int i = 0; i < M; i++){
         scanf("%d %d", &u, &v);
-        Node[u].push_back(v);
-        Node[v].push_back(u);
+        Graph[u].push_back(v);
+        Graph[v].push_back(u);
     }
 
     // Iter
     for(int i = 1; i < N + 1; i++){
         if(!isVisited[i]) {
             count++;
-            DFS(i);
+            DFS(i, count);
         }
     }
 
     // Print
     cout << count;
 }
-
-void DFS(int v) {
-    if(isVisited[v]){
-        return;
-    }
-
-    isVisited[v] = true;
-
-    for(int i : Node[v]){
-        if(isVisited[i] == false){
-            DFS(i);
-        }
-    }
-}   
