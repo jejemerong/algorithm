c79ee924980995923fef5c45f075834ac4042ba6
jejemerong <9708jjw@gmail.com>
2023-10-04 20:56:19 +0900

    Fix: 연결요소 인덱스 수정

--- ps/11724_연결요소.cpp
+++ ps/11724_연결요소.cpp
@@ -4,46 +4,49 @@
 using namespace std;
 
 // Type Def
-static vector<vector<int > > Graph;
-static vector <bool> isVisited;
+static vector<vector <int > > Graph;
+static vector<bool> isVisited;
 
-void DFS(int v, int count) {
-    if(isVisited[v]) return;
+// TODO: DFS static 먼저 선언해서 밑에 작성해도 되게끔
+void DFS(int v, int count)
+{
+    if(isVisited[v]) return; // 만약 방문한 노드면 나가세용
     isVisited[v] = true;
 
-    for(int i = 0; i < Graph[v].size();i++){
-        int y = Graph[v][i];
+    for(int i = 0; i < Graph[v].size(); i++){
+        int y = Graph[v][i]; // 이 노드의 연결된 노드 차례로 방문
         if(isVisited[y] == false) {
+            count++;
             DFS(y, count);
         }
     }
 }
 
-int main()
+int main() 
 {
     // Init
     int N, M;
     scanf("%d %d", &N, &M);
-    int count = 0;
     Graph.resize(N + 1);
-    isVisited = vector<bool>(N+1, false);
+    isVisited = vector<bool> (N+1, false);
 
     // vector Init
-    int u, v;
-    for(int i = 0; i < M; i++){
+    for(int i = 0; i < M; i++){ //연결된 엣지 개수만큼
+        int u, v;
         scanf("%d %d", &u, &v);
         Graph[u].push_back(v);
         Graph[v].push_back(u);
     }
 
-    // Iter
-    for(int i = 1; i < N + 1; i++){
-        if(!isVisited[i]) {
+    int count = 0;
+
+    for(int i = 1; i < isVisited.size(); i++) { // 첫번째 노드부터 조사
+        if(!isVisited[i]){
             count++;
             DFS(i, count);
         }
     }
 
-    // Print
-    cout << count;
+    printf("%d", count);
+    return 0;
 }
