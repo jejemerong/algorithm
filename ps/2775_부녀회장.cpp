64b34e4d54342a2f1656ad7d9227fc393a5fc425
jejemerong <9708jjw@gmail.com>
2023-10-27 17:52:04 +0900

    Refactor: 백준 2775 부녀회장 - 메모리: 2020 KB - 시간: 0 ms - 인원 배열 초기화 후 테스트 케이스 입력으로 수정

--- ps/2775_부녀회장.cpp
+++ ps/2775_부녀회장.cpp
@@ -2,34 +2,25 @@
 
 using namespace std;
 
+static int Wohnung[15][15];
+
 int main()
 {
-    int T;
-    scanf("%d", &T);
-    int Test[T][2];
-    // Test input
-    for(int i = 0; i < T; i++){
-        for(int j = 0; j < 2; j++){
-            scanf("%d", &Test[i][j]); // j: (0, k), (1, n)
+    int T, K, N;
+    // 바로 테스트케이스 입력 받는게 아니라 배열 초기화부터
+    for(int i = 0; i < 15; i++){
+        Wohnung[i][1] = 1;
+        Wohnung[0][i] = i;
+    }
+    for(int i =1; i < 15; i++){
+        for(int j = 2; j < 15; j++){
+            Wohnung[i][j] = Wohnung[i-1][j] + Wohnung[i][j-1];
         }
-        // Complex init
-        int k = Test[i][0];
-        int n = Test[i][1];
-        int Wohnung[k+1][n]; // k 층 n 호
-        for(int a = 0; a <= k; a++){ // k
-            for(int b = 1; b <= n; b++) { // n
-                Wohnung[0][b] = b;
-                Wohnung[a][1] = 1;
-            }
-        }
-
-        for(int c = 1; c <= k; c++){
-            for(int d = 2; d <= n; d++){
-                Wohnung[c][d] = Wohnung[c-1][d] + Wohnung[c][d-1];
-            }
-        }
-        cout << Wohnung[k][n] << "\n";
     }
 
+    scanf("%d", &T);
+    for(int i = 0; i < T; i++){
+        scanf("%d\n%d", &K, &N);
+        printf("%d\n", Wohnung[K][N]);
+    }
 }
-
