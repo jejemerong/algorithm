b65ebca0406c46f9a2e0e16d6fe6f42c6ed2e66e
jejemerong <9708jjw@gmail.com>
2023-10-28 21:34:50 +0900

    Feat: 이항계수 1 다른 풀이 추가 - 메모리: 2020 KB - 시간: 0 ms

--- ps/11050_이항계수1.cpp
+++ ps/11050_이항계수1.cpp
@@ -1,26 +1,13 @@
-#include <iostream>
-
-using namespace std;
-
+#include<iostream>
 int main()
 {
-    int N, K;
-    scanf("%d %d", &N, &K);
-    int Arr[N + 1][N + 1];
-
-    // DP 배열 초기화
-    for(int i = 0; i <= N; i++){
-        // 순서 상관 ㄴㄴㄴㄴ
-        // TODO: 근데 왜 i 가 0 일 때는 왜 1이지? `Arr[i][1] = i;` 때문에 0 이 되어야 하는 거 아닌가?
-        Arr[i][0] = 1;
-        Arr[i][i] = 1;
-        Arr[i][1] = i;
+	int N,K,sum=1;
+	scanf("%d%d",&N,&K);
+	// for(int n = K+1,K = 1;--n;N--) sum=sum*N/(K++);
+    for(int n = K + 1, K = 1; n > 0; n--){
+        sum = sum * N / K;
+        K++;N--;
     }
-
-    for(int i = 2; i <= N; i++){
-        for(int j = 1; j < i; j++){ // 조건 j < i 주의
-            Arr[i][j] = Arr[i - 1][j] + Arr[i - 1][j - 1];
-        }
-    }
-    cout << Arr[N][K];
+	printf("%d\n",sum);
+	return 0;
 }
